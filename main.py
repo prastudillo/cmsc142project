3ae769c6bbb70bf3dd0ca92dc54b4886e877e8f3
paolo <prastudillo@up.edu.ph>
2019-12-04 00:38:11 +0800

    Working complete with GUI and Algo

--- main.py
+++ main.py
@@ -1,25 +1,97 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 from tkinter.filedialog import askopenfilename
+from ttkthemes import ThemedTk
+
+def SubsetSum(integers, sum):
+
+    N = len(integers)
+
+    # combination
+    nopts = [0] * (N + 2)  # nopts[N+2]
+    option = [[0 for x in range(N + 2)] for y in range(N + 2)]  # nopts[N+2][N+2]
+    start = 0
+    move = 0
+    nopts[start] = 1
+    i = 0
+    withSubset = False
+    results.txt.insert("end", "Subsets\n") #output formatting
+
+    while (nopts[start] > 0):
+
+        if (nopts[move] > 0):
+            move += 1
+            nopts[move] = 0
+            if (move != N + 1):
+                for candidate in range(N - 1, -1, -1):
+                    for i in range(move - 1, -1, -1):
+                        if (integers[candidate] <= option[i][nopts[i]]):
+                            break
+                    if i < 1:
+                        nopts[move] += 1
+                        option[move][nopts[move]] = integers[candidate]
+        else:
+            summation = 0
+            for i in range(1, move, 1):
+                summation = summation + option[i][nopts[i]]
+                if (summation > sum):  # stops the loop if greater than sum
+                    break
+            if (summation == sum):
+                results.txt.insert("end", "{") #output formatting
+
+                for i in range(1, move, 1):
+                    withSubset = True
+                    print(str(option[i][nopts[i]]) + " ", end='')
+                    resultStr = str(option[i][nopts[i]]) + " "
+                    results.txt.insert("end",resultStr)
+                print("\n")
+               results.txt.insert("end", "}\n")
+
+            move = move - 1
+            nopts[move] = nopts[move] - 1
+
+    #when there is no available solution
+    if (withSubset == False):
+        results.txt.insert("end", "No subsets possible.\n")
+
+
+
 
 def Solve():
+
+    #clear the results
+    results.txt.config(state='normal')
+    results.txt.delete("1.0", "end")
+
     #get the list of integers from the entry
     integers = intEntry.get()
     integers = integers.split(',')
+
+    # convert contents of integer list to int list
+    integers = [int(i) for i in integers]
+
     print("Integers: " + str(integers))
+    intStr = "Integers: " + str(integers) + "\n"
+    results.txt.insert("end",intStr)
+
 
     #get the sum from the entry
     sum = sumEntry.get()
     print("Sum: " + str(sum))
+    sumStr = "Sum: " + str(sum) + "\n"
+    results.txt.insert("end",sumStr)
 
+    sum = int(sum)
 
     #solve
     #put it in the results box
+    SubsetSum(integers, sum)
 
-
-    print("Solve")
+    results.txt.config(state='disabled')
 
 
 def OpenFile():
+
     #Open File and read contents
     name = askopenfilename(initialdir="~/Desktop/cmsc142project/",
                            filetypes=(("Text File", "*.txt"), ("All Files", "*.*")),
@@ -29,9 +101,15 @@ def OpenFile():
     try:
         # reading text file
         with open(name,'r') as file:
+
+            results.txt.config(state='normal')
+            results.txt.delete("1.0", "end") # clearing of results
+
             noOfSets = file.readline().strip()
             noOfSets = int(noOfSets)
             print("No of sets: " + str(noOfSets))
+            setsStr = "No of sets: " + str(noOfSets) + "\n"
+            results.txt.insert("end",setsStr)
 
             for line in range(noOfSets):
 
@@ -40,59 +118,100 @@ def OpenFile():
                 integers = file.readline().strip()
                 integers = integers.split() #split the integers line and put it in array
 
+                # convert contents of integer list to int list
+                integers = [int(i) for i in integers]
+
+                print("Set: " + str(line + 1))
+                setNoStr = "\n" + "Set: " + str(line + 1) + "\n"
+                results.txt.insert("end",setNoStr)
+
+                # sum for output
+                sumStr = "Sum: " + str(sum) + "\n"
+                results.txt.insert("end", sumStr)
+
+                print("Integers: " + str(integers))
+                intStr = "Integers: " + str(integers) + "\n"
+                results.txt.insert("end", intStr)
 
                 #algorithm
-                #put it in display box
+                SubsetSum(integers, sum)
 
-                print("Set: " + str(line+1))
-                print("Sum: " + str(sum))
-                print("Integers: " + str(integers))
+        results.txt.config(state='disabled')
+
 
 
     except:
         print("No file exists")
+        results.txt.insert("end", "No File exists.")
 
 def clear_search(event):
     intEntry.delete(0,END)
 
+
+# textbox with scrollbar for output
+class TextScrollCombo(ttk.Frame):
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+
+    # ensure a consistent GUI size
+        self.grid_propagate(False)
+    # implement stretchability
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+    # create a Text widget
+        self.txt = tk.Text(self)
+        self.txt.grid(row=0, column=0, sticky="nsew", padx=2, pady=2)
+
+    # create a Scrollbar and associate it with txt
+        scrollb = ttk.Scrollbar(self, command=self.txt.yview)
+        scrollb.grid(row=0, column=1, sticky='nsew')
+
+
 #GUI
-win = tk.Tk()
-win.title("Subset sum solver")
+win = ThemedTk()
+win.get_themes()
+win.set_theme('arc')
+win.config(bg='#F7F8FB')
+win.title("Subset Sum Solver")
 win.geometry("750x750")
 
 #Message describing the problem
-tk.Label(win, text="Subset Sum Solver", font="Helvetica 25 bold").pack(pady=10)
-tk.Label(win, text="Given a set S of unique positive integers and a positive integer k, is there a subset of S that adds up to k?", font="Helvetica 10 italic").pack(pady=10)
+SSSLabel = ttk.Label(win, text="Subset Sum Solver", font="Helvetica 25 bold", foreground='#3898DF').pack()
+ttk.Label(win, text="Given a set S of unique positive integers and a positive integer k, is there a subset of S that adds up to k?", font="Helvetica 10 italic").pack(pady=10)
 
-browseBtn = tk.Button(win, text="Browse input file", command = OpenFile)
+browseBtn = ttk.Button(win, text="Browse input file", command = OpenFile)
 browseBtn.pack(pady=10)
-tk.Label(win, text="or", font="bold").pack(pady=5)
+ttk.Label(win, text="or", font="bold").pack(pady=5)
 
-intLabel = tk.Label(win, text="Enter list of positive integers")
-formatLabel = tk.Label(win, text="Ex. 1,2,3,4,5")
+intLabel = ttk.Label(win, text="Enter list of positive integers")
+formatLabel = ttk.Label(win, text="Ex. 1,2,3,4,5")
 intLabel.pack(pady=10)
 formatLabel.pack()
-intEntry = tk.Entry(win, width="50", justify="center") #List of integers
+intEntry = ttk.Entry(win, width="50", justify="center") #List of integers
 intEntry.insert(0,'1,2,3,4,5') #placeholder
 intEntry.bind("<FocusIn>", lambda args: intEntry.delete(0, 'end'))
 intEntry.pack(pady=10)
 
-sumLabel = tk.Label(win, text="Enter sum", width="200")
+sumLabel = ttk.Label(win, text="Enter sum")
 sumLabel.pack(pady=10)
-sumEntry = tk.Entry(win) #Sum
+sumEntry = ttk.Entry(win, justify="center") #Sum
 sumEntry.pack()
 
-solveBtn = tk.Button(win, text="Solve", command = Solve)
+solveBtn = ttk.Button(win, text="Solve", command = Solve)
 solveBtn.pack(pady=10)
-tk.Label(win,text="").pack(pady=20)
+ttk.Label(win,text="").pack(pady=15)
 
 #Output
-tk.Label(win, text="Results", font="Helvetica 15 bold").pack()
-results = tk.Text(win, height="10")
-results.pack(pady=20)
-results.insert("end","")
-results.config(state="disabled")
+ttk.Label(win, text="Results", font="Helvetica 15 bold", foreground='#3898DF').pack()
+results = TextScrollCombo(win)
+results.pack(fill="both", expand=True, pady=20)
+results.config(width=600, height=600)
 
+results.txt.config(font=("consolas", 12), undo=True, wrap='word')
+results.txt.config(borderwidth=3, relief="sunken")
 
 
 win.mainloop()
